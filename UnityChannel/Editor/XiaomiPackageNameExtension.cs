f5f1024959f6400bfda29dd7556c465ee6aa442e
Unity Technologies <@unity.com>
2019-04-03 12:36:22 +0000

    com.unity.xiaomi@0.0.4

---
+++ UnityChannel/Editor/XiaomiPackageNameExtension.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+
+namespace XiaomiSupport
+{
+    public class XiaomiPackageNameExtension : IPreprocessBuild, IPostprocessBuild
+    {
+        public int callbackOrder
+        {
+            get { return 0; }
+        }
+
+        private const string XiaomiPostfix = ".mi"; // The postfix requested by Xiaomi.
+        private bool IsXiaomiPostfixAdded = false;
+
+        public void OnPreprocessBuild(BuildTarget target, string path)
+        {
+            // Check if the current package name has Xiaomi postfix.
+            var originalPackageName = PlayerSettings.applicationIdentifier;
+            if (EditorUserBuildSettings.selectedBuildTargetGroup == BuildTargetGroup.Android && !originalPackageName.EndsWith(XiaomiPostfix))
+            {
+                PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, originalPackageName + XiaomiPostfix);
+                IsXiaomiPostfixAdded = true;
+            }
+        }
+
+        public void OnPostprocessBuild(BuildTarget target, string path)
+        {
+            if (IsXiaomiPostfixAdded)
+            {
+                var packageName = PlayerSettings.applicationIdentifier;
+                PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, packageName.Remove(packageName.Length - XiaomiPostfix.Length));
+                IsXiaomiPostfixAdded = false;
+            }
+        }
+    }
+}
