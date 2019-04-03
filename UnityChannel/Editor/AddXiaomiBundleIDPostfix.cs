1ddcdf98d6122efec692e6580c0ce572ae14af7a
Unity Technologies <@unity.com>
2019-04-03 12:36:19 +0000

    com.unity.xiaomi@0.0.3

---
+++ UnityChannel/Editor/AddXiaomiBundleIDPostfix.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+
+class AddXiaomiBundleIDPostfix : IPreprocessBuild
+{
+    public int callbackOrder { get { return 0; } }
+    private const string XiaomiPostfix = ".mi"; // The postfix requested by Xiaomi.
+
+    public void OnPreprocessBuild(BuildTarget target, string path)
+    {
+        // Check if the current package name has Xiaomi postfix.
+        if (EditorUserBuildSettings.selectedBuildTargetGroup == BuildTargetGroup.Android && !PlayerSettings.applicationIdentifier.EndsWith(XiaomiPostfix))
+            PlayerSettings.SetApplicationIdentifier(EditorUserBuildSettings.selectedBuildTargetGroup, PlayerSettings.applicationIdentifier + XiaomiPostfix);
+    }
+}
